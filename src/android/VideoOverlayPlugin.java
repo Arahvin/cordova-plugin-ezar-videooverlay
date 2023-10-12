9c66d1899fbd75ddc9c49fbf46c4725161ab4c44
DIOGO-CORREIA\diogo.correia <diogo.correia@inm.pt>
2023-10-12 19:19:33 +0100

    Removing weird package

--- src/android/VideoOverlayPlugin.java
+++ src/android/VideoOverlayPlugin.java
@@ -19,8 +19,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import javax.rmi.CORBA.Util;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;

0d3fdf231f2bfd17d6717179577b7c5b02dc06a2
DIOGO-CORREIA\diogo.correia <diogo.correia@inm.pt>
2023-10-12 19:39:25 +0100

    Converting string to CameraDirection

--- src/android/VideoOverlayPlugin.java
+++ src/android/VideoOverlayPlugin.java
@@ -1176,7 +1176,7 @@ public class VideoOverlayPlugin extends CordovaPlugin implements Camera.PreviewC
 	}
 
 	public void startRecordingVideo() {
-		mCamera = Camera.open(getCameraId("FRONT"));
+		mCamera = Camera.open(getCameraId(CameraDirection.valueOf("FRONT")));
 		mMediaRecorder = new MediaRecorder();
 		mCamera.unlock();
 		mMediaRecorder.setCamera(mCamera);
